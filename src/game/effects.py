46c0c1722302406b47b21361bc999b7948bc262a
David Veen <veen@pmtgroep.nl>
2017-08-10 17:25:19 +0200

    minor changes to effects

--- src/game/effects.py
+++ src/game/effects.py
@@ -16,6 +16,42 @@ def recruit
 # get leader
 # post messages
 
+def team_vote(game: model.Game):
+    """
+    Game is in team_vote status
+    send the following notifications:
+    PUBLIC
+        - PROPOSAL_VOTE_START
+    PRIVATE
+        - PROPOSAL_VOTE_ALERT
+    
+    notifications are skipped if proposal is 6th proposal
+    """
+    pass
+
+
+def team_vote_complete(game: model.Game):
+    """
+    Game is in team_vote_complete status
+    send the following notifications:
+    PUBLIC
+        - PROPOSAL_VOTE_RESULT
+        OR if 6th proposal
+        - PROPOSAL_ACCEPTED_BY_DEFAULT
+    """
+    pass
+
+
+def mission_vote(game: model.Game):
+    """
+    Game is in mission_vote status
+    send the following notifications:
+    PUBLIC
+        - MISSION_VOTE_START
+    PRIVATE
+        - 
+    """   
+
 
 def assign_player_positions(game_id: int):
     pass
